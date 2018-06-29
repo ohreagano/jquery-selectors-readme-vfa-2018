512baa3379d93aa4eaff0aebb5065decbc84e24b
Reagan Cline <reaganecline@gmail.com>
2018-06-29 14:13:42 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+
+function paragraphSelector() {
+  $('p')
+}
+
+function lastImageSelector() {
+   $('img:last')
+}
+
+function ninjaBabySelector() {
+   $('#baby-ninja')
+}
+
+function divSelector() {
+   $('.pics')
+}
+
+function firstListItem() {
+   $('#pic-list img:first-child')
+}
